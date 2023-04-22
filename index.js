88be51a42adcf767444a67558f816f4f8d482ffd
EthanRHanna <42752797+EthanRHanna@users.noreply.github.com>
2023-04-22 16:20:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.get("/rest/ticket/:id", function (req, res) {
 });
 
 //A Delete request
-app.delete("/rest/delticket/:id", function (req, res) {
+app.delete("/rest/delete/:id", function (req, res) {
   async function run() {
     const query = { _id: parseInt(req.params.id) };
 
